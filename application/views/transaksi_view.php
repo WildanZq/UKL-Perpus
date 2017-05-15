0205eae5427a92aedada4bd7952c0ec8ac5ecddf
WildanZq <wildan2wildan@gmail.com>
2017-05-15 12:50:28 +0700

    Deadline and denda updated

--- application/views/transaksi_view.php
+++ application/views/transaksi_view.php
@@ -16,28 +16,38 @@
                             <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
                                 <thead>
                                     <tr>
-                                        <th>Nama Peminjam</th>
-                                        <th>Judul Buku</th>
+                                        <th>Denda / Deadline</th>
                                         <th>Tanggal Pinjam</th>
-                                        <th>Denda</th>
+                                        <th>Nama Peminjam</th>
+                                        <th>Barcode</th>
+                                        <th>Judul Buku</th>
                                         <th>Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                   <?php foreach ($peminjaman as $pinjam): ?><tr>
-                                      <td><?php echo $pinjam->NAMA; ?></td>
-                                      <td><?php echo $pinjam->JUDUL; ?></td>
-                                      <td><?php $tanggal = date_create($pinjam->TANGGAL);echo date_format($tanggal,"d M Y"); ?></td>
-                                      <td><?php $deadline = strtotime("+7 day",strtotime($pinjam->TANGGAL));
-                                      if(strtotime(date("Y-m-d")) > $deadline) {
-                                        $today = time();
-                                        $diff = $today - $deadline;
-                                        echo 'Rp '.floor($diff / 86400)*500;
-                                      } else { echo '0'; } ?></td>
-                                      <td>
-                                        <a href='<?php echo base_url(); ?>index.php/transaksi/kembali/<?php echo $pinjam->NO_PINJAM; ?>' type='button' class='btn btn-success'>Kembali</a>
-                                      </td>
-                                    </tr>
+                                    <td><?php
+                                    $deadline = strtotime("+7 day",strtotime($pinjam->TANGGAL));
+                                    $today = time();
+
+                                    if(strtotime(date("Y-m-d")) > $deadline) {
+                                      $diff = $today - $deadline;
+                                      echo 'Rp '.floor($diff / 86400)*500;
+                                    } else if(strtotime(date("Y-m-d")) < $deadline) {
+                                      $diff = $deadline - $today;
+                                      echo (floor($diff / 86400)+1).' Hari lagi ';
+                                    } else {
+                                      echo 'Hari ini deadline';
+                                    } ?></td>
+                                    <td><?php $tanggal = date_create($pinjam->TANGGAL);echo date_format($tanggal,"d M Y"); ?></td>
+                                    <td><?php echo $pinjam->NAMA; ?></td>
+                                    <td><?php echo $pinjam->BARCODE; ?></td>
+                                    <td><?php echo $pinjam->JUDUL; ?></td>
+                                    <td>
+                                      <a href='<?php echo base_url(); ?>index.php/transaksi/kembali/<?php echo $pinjam->NO_PINJAM; ?>' type='button' class='btn btn-success'>Kembali</a>
+                                      <a href='<?php echo base_url(); ?>index.php/transaksi/hilang/<?php echo $pinjam->NO_PINJAM; ?>' type='button' class='btn btn-danger'>Hilang</a>
+                                    </td>
+                                  </tr>
                                   <?php endforeach; ?></tbody>
                             </table>
                             <!-- /.table-responsive -->
